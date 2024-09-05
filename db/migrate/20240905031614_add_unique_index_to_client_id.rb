b144e3bb80f677fbae2b006780d3ee22b02780db
rami boussarsar <ramibousarsar@gmail.com>
2024-09-04 23:17:17 -0400

    Add unique index to client_id

--- db/migrate/20240905031614_add_unique_index_to_client_id.rb
+++ db/migrate/20240905031614_add_unique_index_to_client_id.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToClientId < ActiveRecord::Migration[7.1]
   def change
-    add_index :clients, :client_id, unique: true
+    add_index :clients, :client_id, unique: true, if_not_exists: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_04_022632) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_031614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_04_022632) do
     t.string "store_location"
     t.string "gender"
     t.bigint "manager_id", null: false
+    t.index ["client_id"], name: "index_clients_on_client_id", unique: true
   end
 
   create_table "interactions", force: :cascade do |t|
