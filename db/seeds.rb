821fab18fbb6039252bd9f89264a35c0d746f599
rami boussarsar <ramibousarsar@gmail.com>
2024-09-05 21:35:31 -0400

    Update seeds.rb with new client creation logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,8 @@ end
 manager_one = staff_members.find { |staff| staff.role == "manager" && staff.name == "Manager One" }
 manager_two = staff_members.find { |staff| staff.role == "manager" && staff.name == "Manager Two" }
 
-# Create 20 random clients with varying purchase totals and assign them to staff members
-clients_data = 10.times.map do
+# Create 100 random clients with varying purchase totals and assign them to staff members
+clients_data = 20.times.map do
   {
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -54,24 +54,32 @@ clients.each do |client|
   Purchase.create!(purchase_data)
 end
 
-# Create some appointments, wishlist items, and campaigns for completeness
+# Create more appointments for clients
 appointments_data = [
   { client: clients[0], title: 'Consultation', scheduled_at: '2024-06-01', staff: staff_members[0] },
-  { client: clients[1], title: 'Follow-up', scheduled_at: '2024-07-01', staff: staff_members[1] }
+  { client: clients[1], title: 'Follow-up', scheduled_at: '2024-07-01', staff: staff_members[1] },
+  { client: clients[2], title: 'New Product Meeting', scheduled_at: '2024-09-01', staff: staff_members[2] },
+  { client: clients[3], title: 'Annual Review', scheduled_at: '2024-10-01', staff: staff_members[0] }
 ]
 
 appointments_data.each { |data| Appointment.create!(data) }
 
+# Create more wishlist items for clients
 wishlist_items_data = [
   { client: clients[2], item_name: 'Wishlist Item 1', item_reference: 'REF123' },
-  { client: clients[3], item_name: 'Wishlist Item 2', item_reference: 'REF456' }
+  { client: clients[3], item_name: 'Wishlist Item 2', item_reference: 'REF456' },
+  { client: clients[4], item_name: 'Wishlist Item 3', item_reference: 'REF789' },
+  { client: clients[5], item_name: 'Wishlist Item 4', item_reference: 'REF101' }
 ]
 
 wishlist_items_data.each { |data| WishlistItem.create!(data) }
 
+# Create more campaigns for the store
 campaigns_data = [
   { name: 'Summer Sale', start_date: '2024-08-01', end_date: '2024-08-31', staff: staff_members[0] },
-  { name: 'Winter Sale', start_date: '2024-12-01', end_date: '2024-12-31', staff: staff_members[0] }
+  { name: 'Winter Sale', start_date: '2024-12-01', end_date: '2024-12-31', staff: staff_members[0] },
+  { name: 'Spring Promotion', start_date: '2024-04-01', end_date: '2024-04-30', staff: staff_members[1] },
+  { name: 'Black Friday', start_date: '2024-11-25', end_date: '2024-11-29', staff: staff_members[1] }
 ]
 
 campaigns = campaigns_data.map do |data|
@@ -79,9 +87,12 @@ campaigns = campaigns_data.map do |data|
   Campaign.create!(data)
 end
 
+# Assign more clients to campaigns
 campaign_assignments_data = [
   { client_id: clients[4].id, campaign_id: campaigns[0].id, staff_id: staff_members[2].id },
-  { client_id: clients[3].id, campaign_id: campaigns[1].id, staff_id: staff_members[2].id }
+  { client_id: clients[3].id, campaign_id: campaigns[1].id, staff_id: staff_members[2].id },
+  { client_id: clients[5].id, campaign_id: campaigns[2].id, staff_id: staff_members[0].id },
+  { client_id: clients[6].id, campaign_id: campaigns[3].id, staff_id: staff_members[1].id }
 ]
 
 campaign_assignments_data.each { |data| CampaignAssignment.create!(data) }
