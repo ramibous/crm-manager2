a892330fd2c5cf511ff2c1529b2815c19fb6619a
rami boussarsar <ramibousarsar@gmail.com>
2024-09-05 21:09:34 -0400

    Fix wkhtmltopdf path for Heroku

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,9 @@
-WickedPdf.config = {
-  exe_path: '/Users/ramiboussarsar/.rbenv/shims/wkhtmltopdf'
-}
+if Rails.env.production?
+  WickedPdf.config = {
+    exe_path: '/app/bin/wkhtmltopdf' # Heroku path for wkhtmltopdf
+  }
+else
+  WickedPdf.config = {
+    exe_path: '/Users/ramiboussarsar/.rbenv/shims/wkhtmltopdf' # Your local path
+  }
+end
