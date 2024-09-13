0233cac5a8c1a1fe34a02da1e167279873221e6f
rami boussarsar <ramibousarsar@gmail.com>
2024-09-12 23:55:59 -0400

    Clear and recompile assetws

--- app/javascript/controllers/timeline_controller.js
+++ app/javascript/controllers/timeline_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
         this.totalItems = parseInt(this.data.get("totalItemsValue"), 10);
         console.log("Timeline items loaded:", this.timelineItems);
       } else {
-        console.error("No timeline items found");
+        
         this.timelineItems = [];
         this.totalItems = 0;
       }
@@ -25,21 +25,21 @@ export default class extends Controller {
       if (this.hasSeeMoreButtonTarget) {
         this.checkSeeMoreButton();
       } else {
-        console.error("See More button target not found");
+       
       }
 
     } catch (error) {
-      console.error("Error connecting controller", error);
+      
     }
   }
 
   loadTimelineItems() {
     if (!this.timelineItems || this.timelineItems.length === 0) {
-      console.error("No timeline items to load.");
+      
       return;
     }
 
-    console.log("Loading timeline items...");
+   
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = this.currentPage * this.itemsPerPage;
     const itemsToLoad = this.timelineItems.slice(startIndex, endIndex);
@@ -88,18 +88,18 @@ export default class extends Controller {
 
   checkSeeMoreButton() {
     if (!this.timelineItems || this.timelineItems.length === 0) {
-      console.log("No timeline items to load.");
+     
       if (this.hasSeeMoreButtonTarget) {
         this.seeMoreButtonTarget.style.display = "none";
       } else {
-        console.error("See More button target not found.");
+       
       }
     } else {
       const shouldHideButton = this.currentPage * this.itemsPerPage >= this.totalItems;
       if (this.hasSeeMoreButtonTarget) {
         this.seeMoreButtonTarget.style.display = shouldHideButton ? "none" : "block";
       } else {
-        console.error("See More button target not found.");
+        
       }
     }
   }
